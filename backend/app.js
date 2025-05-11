694792754c3418289c76a28fdf040b2f89c5fc8d
manish mandal <76776194mm@gmail.com>
2025-05-12 01:10:24 +0530

    completed user registration functionality

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //{ extended: true } ->Allows parsing of nested objects (like fullname.firstname).
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
